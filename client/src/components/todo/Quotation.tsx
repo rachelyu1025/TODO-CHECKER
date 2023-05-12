eeb0b58ad3792a088b002c8f4591262aa958d56a
rachelyu1025 <rachelyu.1025@gmail.com>
2023-05-12 23:43:27 +0900

    💄 [style] Quotation, Progress 컴포넌트 구현

---
+++ client/src/components/todo/Quotation.tsx
@@ -0,0 +1,11 @@
+export default function Quotation() {
+  return (
+    <div className='flex justify-center mt-4'>
+      <div className='text-center w-[85%] py-2 tablet:py-4 rounded-sm bg-[#c0dcfe78]'>
+        <p className='text-lg font-medium text-center tablet:text-xl'>
+          ❛ Hey Kiyeon ❜
+        </p>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Todo.tsx
+++ client/src/pages/Todo.tsx
@@ -1,16 +1,29 @@
 import { StarProvider } from '../context/StarContext';
 import TodoInput from '../components/todo/TodoInput';
 import TodoList from '../components/todo/TodoList';
+import Quotation from '../components/todo/Quotation';
+import Progress from '../components/todo/Progress';
 
 export default function Todo() {
   return (
-    <main className='w-full p-3 m-3 bg-[#fff] h-1/2'>
-      <TodoInput />
-      <ul className='m-2 overflow-scroll h-1/3'>
-        <StarProvider>
-          <TodoList />
-        </StarProvider>
-      </ul>
+    <main className='relative w-full p-3 m-3 h-1/2'>
+      <h1 className='absolute left-0 text-3xl font-semibold top-1'>TODO</h1>
+
+      {/* Progress bar */}
+      <Progress />
+
+      {/* Quotation */}
+      <Quotation />
+
+      {/* Todo list */}
+      <div className='mt-4 bg-[#fff] rounded-b-md'>
+        <TodoInput />
+        <ul className='m-2 overflow-scroll h-1/3'>
+          <StarProvider>
+            <TodoList />
+          </StarProvider>
+        </ul>
+      </div>
     </main>
   );
 }
