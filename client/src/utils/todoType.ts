54a6fba1d8383959382c63d5488d2efeb832c195
rachelyu1025 <rachelyu.1025@gmail.com>
2023-05-17 22:31:07 +0900

    ✨ [feat] interface 모듈화

--- client/src/utils/todoType.ts
+++ client/src/utils/todoType.ts
@@ -10,3 +10,29 @@ export interface TodoType {
     labelColor: string;
   }
 }
+
+export interface StarType {
+  todoId : number;
+  importance: boolean;
+}
+
+export interface PenIconType {
+  todoId: number | undefined;
+  onClick: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export interface CreateType {
+  content: string;
+  createdAt: string; 
+  labelTitle: string; 
+  labelColor: string;
+  setLabel?:React.Dispatch<React.SetStateAction<string>>
+  setContent?:  React.Dispatch<React.SetStateAction<string>>
+}
+
+export interface ModifyType {
+  todoId: number | undefined;
+  content: string;
+  handleModify?: React.Dispatch<React.SetStateAction<boolean>>
+}
+
