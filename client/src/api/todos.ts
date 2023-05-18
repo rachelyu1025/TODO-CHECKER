1a2d1bbfe01ee1b4f8a7c720e9823fcf9d31c654
rachelyu1025 <rachelyu.1025@gmail.com>
2023-05-18 22:42:09 +0900

    ✨ [feat] todolist CRUD 요청함수 로직 및 hooks 구현

--- client/src/api/todos.ts
+++ client/src/api/todos.ts
@@ -1,5 +1,37 @@
+import { ModifyType, CreateType } from './../utils/todoType';
 import api from '../utils/api';
 
-export const getTodos = async() => {
-  return api.get('/todo/20230425').catch(err => console.log(err))
+export const getTodos = async(date: string) => {
+  return api.get(`/todo/${date}`).catch(err => console.log(err))
+}
+
+export const postTodo = async({content, createdAt, labelTitle, labelColor}:CreateType) =>{
+  const body = {
+    content,
+    createdAt,
+    labelTitle,
+    labelColor
+  }
+  
+  return api.post(`/todo`, body).catch(err => console.log(err))
+}
+
+export const putImportance = async({todoId, importance}: {todoId: number; importance: boolean}) =>{
+  const body = {
+    importance
+  }
+  
+  return api.put(`/todo/${todoId}`, body).catch(err => console.log(err))
+}
+
+export const deleteTodo = async({todoId}:{todoId : number | undefined}) =>{
+  return api.delete(`/todo/${todoId}`).catch(err => console.log(err))
+}
+
+export const putTodo = async({todoId, content}:ModifyType) =>{
+  const body = {
+    content
+  }
+
+  return api.put(`/todo/${todoId}`, body).catch(err => console.log(err))
 }
